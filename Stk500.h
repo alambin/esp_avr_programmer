86f55e6fb85e8edeb76918e1a26a1a1e1e4605f6
Alexey Lambin <extern.aleksei.lambin@porsche.de>
2021-02-03 19:45:23 +0300

    1. Not library anymore 2. Import clang-format file 3. Reformat sources 4. Clean up unnecessary includes

---
+++ Stk500.h
@@ -0,0 +1,33 @@
+#ifndef STK500_H
+#define STK500_H
+
+#include <Arduino.h>
+
+class Stk500
+{
+public:
+    Stk500(Stream* serial, int resPin, Print* log = nullptr);
+    bool setupDevice();
+    bool flashPage(byte* loadAddress, byte* data);
+    int  exitProgMode();
+
+private:
+    void resetMCU();
+    int  getSync();
+    int  enterProgMode();
+    int  loadAddress(byte adrHi, byte adrLo);
+    int  setProgParams();
+    int  setExtProgParams();
+
+    byte execCmd(byte cmd);
+    byte execParam(byte cmd, byte* params, int count);
+    byte sendBytes(byte* bytes, int count);
+    int  waitForSerialData(int dataCount, int timeout);
+    int  getFlashPageCount(byte flashData[][131]);
+
+    int     _resetPin;
+    Stream* _serial;
+    Print*  _log;
+};
+
+#endif  // STK500_H
---
+++ esp_avr_programmer.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include "WebServ.h"
+
+#define RESET_PIN 4
+
+const char* ssid     = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+WiFiServer server(80);
+
+WebServ webServ(RESET_PIN);
+
+void
+setup()
+{
+    pinMode(RESET_PIN, OUTPUT);
+    digitalWrite(RESET_PIN, 1);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+
+    server.begin();
+}
+
+void
+loop()
+{
+    WiFiClient client = server.available();
+
+    if (client) {
+        String line     = client.readStringUntil('\r');
+        int    httpCmd  = webServ.GetCommand(line);
+        String urlParam = webServ.GetUrlParam(line);
+
+        if (httpCmd == webServ.httpCmdIndex) {
+            webServ.WSCmdIndex(&client);
+        }
+        else if (httpCmd == webServ.httpCmdDelete) {
+            webServ.WSCmdDelete(&client, urlParam);
+        }
+        else if (httpCmd == webServ.httpCmdFlash) {
+            webServ.WSCmdFlash(&client, urlParam);
+        }
+        else if (httpCmd == webServ.httpCmdList) {
+            webServ.WSCmdList(&client);
+        }
+        else if (httpCmd == webServ.httpCmdUpload) {
+            webServ.WSCmdUpload(&client, urlParam);
+        }
+        else {
+            webServ.WSCmdIndex(&client);
+        }
+
+        client.flush();
+        while (client.available())
+            client.read();
+        client.stop();
+    }
+}
+
+void
+formatDevice()
+{
+    SPIFFS.begin();
+    SPIFFS.format();
+    SPIFFS.end();
+}
